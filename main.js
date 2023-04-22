a505a637e99ef8c68eaa223340369b11ce3d3ef9
Aadil Ahmed <aadilahmed0@gmail.com>
2023-04-21 23:19:17 -0700

    completed lab

--- main.js
+++ main.js
@@ -3,6 +3,38 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const modal = document.getElementById("modal");
+modal.classList.add("hidden");
+
+const likeButtons = document.querySelectorAll(".like-glyph");
+likeButtons.forEach(handleLike);
+
+function handleLike(item) {
+  item.addEventListener('click', function (e) {
+    e.preventDefault();
+    if(e.target.innerText === EMPTY_HEART){
+      mimicServerCall()
+      .then(() => {
+        e.target.innerText = FULL_HEART;
+        e.target.classList.add("activated-heart");
+      })
+      .catch((error) => {
+        document.getElementById("modal").classList.remove("hidden");
+        document.getElementById("modal-message").innerText = error.message;
+        setTimeout(() => document.getElementById("modal").classList.add("hidden"), 3000);
+      })      
+    }
+    else{
+      e.target.innerText = EMPTY_HEART;
+      e.target.classList.remove("activated-heart");
+    }   
+  });
+}
+
+
+
+
+
 
 
 
